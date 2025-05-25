757598b2b5426de5514f44296ff6ff083fee7e6f
Gabriel Arcila <gabriel_jesus1122@hotmail.com>
2025-05-25 16:46:42 -0400

    creation of the distribution binomial class

--- calspy/Estadistica/Distribution.py
+++ calspy/Estadistica/Distribution.py
@@ -1,14 +1,19 @@
-from math import sqrt
+from math import sqrt,factorial
 
 class Distribution:
     def __init__(self,values:list,probabilities:list):
         self.values = values
         self.probabilities = probabilities
+        self.list_distribution_function = list()
+        self.mean = 0
+        self.variance = 0
+        self.standard_deviation = 0
         self.check_types()
         self.check_probabilities()
+        
 
     def check_types(self):
-        band_1 = all(isinstance(i, (int, float, str)) for i in self.values)
+        band_1 = all(isinstance(i, (int, float)) for i in self.values)
         band_2 = all(isinstance(i, (int, float)) for i in self.probabilities)
         self.is_valid_types = band_1 and band_2
         return self.is_valid_types
@@ -60,12 +65,46 @@ class Distribution:
             self.calculate_variance()
             self.standard_deviation = sqrt(self.variance)
             return self.standard_deviation
+    
 
-
+class Distribution_Binomial (Distribution):
+    def __init__ (self,value_success,value_failure,probability_success,probability_failure):
+        values = [value_success, value_failure]
+        probabilities = [probability_success,probability_failure]
+        super().__init__(values,probabilities)
+    
+    def distribution_bernoulli(self,num_experiments:int,num_successes:int,kind:str):
+        if super().check_probabilities() and super().check_types():
+            numerator = factorial(num_experiments)
+            denominator = factorial(num_successes) * factorial(num_experiments - num_successes)
+            combinatorial_number = numerator / denominator
+            probability_success = self.probabilities[0] ** num_successes
+            probability_failure = self.probabilities[1] ** (num_experiments - num_successes)
+            bernoulli = combinatorial_number * probability_success * probability_failure
+            if (kind == "success"):
+                return bernoulli
+            elif (kind == "failure"):
+                return (1 - bernoulli)
+            else:
+                return 0
+        return 0
+    
+    def calculate_mean(self,num_experiments:int):
+        self.mean = num_experiments * self.probabilities[0]
+        return self.mean
+    
+    def calculate_variance(self,num_experiments:int):
+        self.variance = num_experiments * self.probabilities[0] * self.probabilities[1]
+        return self.variance
+    
+    def calculate_standard_deviation(self,num_experiments:int):
+        self.standard_deviation = sqrt(self.calculate_variance(num_experiments))
+        return self.standard_deviation
 
 probability = (1/6)
 a = Distribution([1, 2, 3,4,5,6],[probability for i in range(6)])
 b = Distribution([1, 2, 3,4,5,6],[probability for i in range(6)])
+c = Distribution_Binomial(1,2,0.5,0.5)
 
 print(a.probabilities)
 print(a.values)
@@ -76,3 +115,12 @@ print(a.distribution_function())
 print(a.calculate_mean())
 print(a.calculate_variance())
 print(a.calculate_standard_deviation())
+
+print("-------------------------------------")
+
+print(c.distribution_bernoulli(10,8,"success"))
+print(c.distribution_bernoulli(10,8,"failure"))
+print(c.distribution_bernoulli(10,8,"success") + c.distribution_bernoulli(10,8,"failure"))
+print(c.calculate_mean(10))
+print(c.calculate_variance(10))
+print(c.calculate_standard_deviation(10))
